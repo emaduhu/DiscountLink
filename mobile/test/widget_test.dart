999b4ae3fdbd110fbf030fccff5cf6c54fa05f8e
Evaristus Maduhu <evaristus.maduhu@ega.go.tz>
2026-07-10 15:54:21 +0300

    Fix widget test after app rename

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,9 +1,18 @@
+import 'package:discount_link/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:discountlink/main.dart';
 
 void main() {
   testWidgets('renders Discount Link login', (tester) async {
-    await tester.pumpWidget(const DiscountLinkApp());
-    expect(find.text('Discount Link'), findsOneWidget);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginPage(
+          client: ApiClient('https://example.test'),
+          onSignedIn: (_, _) {},
+        ),
+      ),
+    );
+
+    expect(find.text('Welcome back'), findsOneWidget);
   });
 }
