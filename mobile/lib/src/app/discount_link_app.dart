c23d9f610d79509117900398adc7c90fc62d4d8f
Evaristus Maduhu <evaristus.maduhu@ega.go.tz>
2026-08-07 13:02:31 +0300

    Add DiscountLink app widget

---
+++ mobile/lib/src/app/discount_link_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'discount_link_app_state.dart';
+
+class DiscountLinkApp extends StatefulWidget {
+  const DiscountLinkApp({super.key});
+  @override
+  State<DiscountLinkApp> createState() =>
+      DiscountLinkAppState<DiscountLinkApp>();
+}
