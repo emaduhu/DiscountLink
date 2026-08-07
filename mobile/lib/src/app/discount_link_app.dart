b794f0c49720a7bf45c5f640f5c5a9e7339ff4d1
Evaristus Maduhu <evaristusmaduhu@evaristus-maduhus-MacBook-Pro.local>
2026-08-07 14:00:05 +0300

    Fix DiscountLinkApp wrapper

--- mobile/lib/src/app/discount_link_app.dart
+++ mobile/lib/src/app/discount_link_app.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
-
-import 'discount_link_app_state.dart';
-
-class DiscountLinkApp extends StatefulWidget {
-  const DiscountLinkApp({super.key});
-  @override
-  State<DiscountLinkApp> createState() =>
-      DiscountLinkAppState<DiscountLinkApp>();
-}
+export '../../main.dart' show DiscountLinkApp;
